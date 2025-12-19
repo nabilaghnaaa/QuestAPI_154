66342a1380fba5b5d21423304e5225ce417e5d60
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-19 10:31:38 +0700

    Menambahkan koneksi repository dengan service API siswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,5 +1,8 @@
 package com.example.localrestapi.repositori
 
+import com.example.localrestapi.apiservice.ServiceApiSiswa
+import com.example.localrestapi.modeldata.DataSiswa
+
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
