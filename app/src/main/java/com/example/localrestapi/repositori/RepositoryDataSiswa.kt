814d740d25f28e2920b1b0f68e3f17ee88ffa346
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-19 10:29:19 +0700

    Menambahkan interface repository data siswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,6 @@
 package com.example.localrestapi.repositori
 
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
