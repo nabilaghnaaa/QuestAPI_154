8f32ad281067df6c9fc29c2c6776e3c327d15a0d
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 21:18:52 +0700

    Membuat host navigasi aplikasi

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -6,3 +6,17 @@ import androidx.compose.runtime.Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        ...
+    }
+}
