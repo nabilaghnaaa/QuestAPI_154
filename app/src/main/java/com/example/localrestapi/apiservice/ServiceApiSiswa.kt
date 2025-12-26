6e5dad3296791f36a7982192a40d4c668bb560e9
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 20:33:35 +0700

    membuat ServiceApiSiswa.kt

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -1,2 +1,15 @@
 package com.example.localrestapi.apiservice
 
+import com.example.localrestapi.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa{
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body data: DataSiswa):retrofit2.Response<Void>
+
+}
