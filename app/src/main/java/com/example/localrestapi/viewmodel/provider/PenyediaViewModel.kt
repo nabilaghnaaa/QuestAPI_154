fa696e9d0ceb4b2f9f71f7aafea400e7aa1e7600
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 23:38:38 +0700

    Menambahkan ViewModel Factory untuk seluruh halaman siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -10,3 +10,25 @@ import com.example.localrestapi.viewmodel.DetailViewModel
 import com.example.localrestapi.viewmodel.EditViewModel
 import com.example.localrestapi.viewmodel.EntryViewModel
 import com.example.localrestapi.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
+        }
+    }
+}
