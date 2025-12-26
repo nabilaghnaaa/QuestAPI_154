f3d16a99b364ce211bb18f82476074700b3bdac2
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 21:45:21 +0700

    menambahkan import untuk HalamanEdit.kt

--- app/src/main/java/com/example/localrestapi/view/HalamanEdit.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanEdit.kt
@@ -1,2 +1,14 @@
 package com.example.localrestapi.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.localrestapi.uicontroller.route.DestinasiEdit
+import com.example.localrestapi.viewmodel.EditViewModel
+import com.example.localrestapi.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
