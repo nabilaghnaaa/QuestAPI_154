c5b63fd70e707638e8427fc8cbd6b2a0a0cf1119
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 21:48:49 +0700

    menambahkan fungsi entrysiswascreen

--- app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
@@ -20,3 +20,43 @@ import com.example.localrestapi.uicontroller.route.DestinasiEntry
 import com.example.localrestapi.viewmodel.EntryViewModel
 import com.example.localrestapi.viewmodel.provider.PenyediaViewModel
 import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
