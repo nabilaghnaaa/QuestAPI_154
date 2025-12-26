cb17eb3c2a5a20c46f71695cb50fb61bd94095c9
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 21:17:16 +0700

    Menambahkan fungsi DataSiswaApp sebagai tampilan utama aplikasi

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,8 @@
 package com.example.localrestapi.uicontroller
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
