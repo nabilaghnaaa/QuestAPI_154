9fe6ef8b104f52f0e090f6c293913ed26c864a86
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-26 21:28:22 +0700

    membuat object DestinasiDetail.kt

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -1,4 +1,10 @@
 package com.example.localrestapi.uicontroller.route
 
-object DestinasiDetail {
+import com.example.localrestapi.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
