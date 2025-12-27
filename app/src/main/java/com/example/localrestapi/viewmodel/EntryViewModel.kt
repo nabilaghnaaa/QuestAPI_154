9c55616313a755a196e609128041ccca0638438b
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:47:58 +0700

    menambahkan import di EntryViewModel.kt

--- app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
 class EntryViewModel {
 }
