05279b57e77b455dab8f382c20d60705fa84ae26
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 12:22:38 +0700

    mengubah divider menjadi HorizontalDivider

--- app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
@@ -140,7 +140,7 @@ fun FormTambahSiswa(
         }
     }
 
-    Divider(
+    HorizontalDivider(
         thickness = dimensionResource(R.dimen.padding_small),
         modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
     )
