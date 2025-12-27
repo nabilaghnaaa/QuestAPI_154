f3bbcf44254075568ac39344da8b0700ed51fcec
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 12:24:06 +0700

    Import Icons.Default.ArrowBack dihapus karena tidak digunakan

--- app/src/main/java/com/example/localrestapi/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/localrestapi/view/SiswaTopAppBar.kt
@@ -2,7 +2,6 @@ package com.example.localrestapi.view
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
