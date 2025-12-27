2d2c64c25de5c7cf29071f6cc9a18847ef959534
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:50:51 +0700

    menambahkan import di HomeViewModel.kt

--- app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
