785020d68f3466cff59675d6975cefa03bba2e73
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:47:13 +0700

    Menambahkan proses update data siswa dengan validasi input

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -37,4 +37,16 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
 }
