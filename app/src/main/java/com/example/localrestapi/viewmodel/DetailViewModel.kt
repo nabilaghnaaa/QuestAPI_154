e0aa94cd565537a9bd6b232b1dd93b99a08ed7a5
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:36:48 +0700

    menambahkan import di DetailViewModel.kt

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.localrestapi.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+import retrofit2.HttpException
+import retrofit2.Response
