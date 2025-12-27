7a976906f4fa0e863b4acd96b8355ba795a34ef7
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 12:21:15 +0700

    melakukan perbaikan ke huruf besar sesuai standar kotloin

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -5,6 +5,6 @@ import com.example.localrestapi.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-    const val itemIdArg = "idSiswa"
-    val routeWithArgs = "$route/{$itemIdArg}"
+    const val ITEM_ID_ARG = "idSiswa"
+    val routeWithArgs = "$route/{$ITEM_ID_ARG}"
 }
