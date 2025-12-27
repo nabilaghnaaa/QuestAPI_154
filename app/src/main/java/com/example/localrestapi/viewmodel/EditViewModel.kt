9e8681ba4e542e9754c81f9494439ab4e89fd1e4
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:46:25 +0700

    Menambahkan validasi input data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -31,4 +31,10 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
     fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
