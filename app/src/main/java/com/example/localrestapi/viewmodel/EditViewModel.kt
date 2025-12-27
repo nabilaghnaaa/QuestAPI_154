1b7126d4e4f4891b2c2b4fc4b3fd7d93b1d344d8
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-27 11:42:34 +0700

    menambahkan import ke EditViewModel.kt

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.localrestapi.viewmodel
 
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toUiStateSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
 class EditViewModel {
 }
